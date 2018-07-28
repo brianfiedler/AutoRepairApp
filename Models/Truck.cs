1b552648678f028b1531e5f9d93fe341265bba84
Fiedler Jr <bfiedlerjr93@gmail.com>
2018-07-28 10:50:03 -0700

    Plan

--- Models/Truck.cs
+++ Models/Truck.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace WebApplication1.Models
+namespace WebApplication1.Controllers
 {
     public class Truck
     {
@@ -11,6 +11,10 @@ namespace WebApplication1.Models
         public bool LongBed { get; set; }
         public bool Diesel { get; set; }
         public bool BedCover { get; set; }
+        public bool CDLRequired { get; set; }
+        public string EngineSize { get; set; }
+        public bool LightBar { get; set; }
+        public bool AllWheelDrive { get; set; }
 
 
 
